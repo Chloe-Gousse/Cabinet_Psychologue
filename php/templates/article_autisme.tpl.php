17e8604bd805401c32ba6ae2c5a8ef26c715b5fd
Chloe-Gousse <goussechloe@gmail.com>
2020-06-02 13:00:37 +0200

    article autisme ok

--- php/templates/article_autisme.tpl.php
+++ php/templates/article_autisme.tpl.php
@@ -3,17 +3,85 @@
                     <div class="container">
                         <div class="row">
                             <div class="section-title padd-15">
-                                <h2>Article Autisme</h2>
+                                <h2>Spécialiste du Développement <br>enfant-adolescent-adulte</h2>
                             </div>
                         </div>
                         <div class="row">
+
+                            <div class="article-img">
+                                <img src="../images/Thematiques/Articles/article_autisme_resize" alt="article_autisme">
+                            </div>
                             
                             <article>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta nam sapiente quidem, quae dolor debitis quam cumque minus cupiditate eaque facilis totam corrupti provident odio et expedita quo dolorum ratione?
-                                </p>
+                                <p><strong>Je pratique les diagnostics à tous les âges de la vie, enfants, adolescents, adultes, dont le syndrome d’Asperger, ce dernier terme étant toujours utilisé alors que depuis le DSM5 (2013, en France), il fait également partie des TSA.</strong></p>
+
+ 
+
+                                <h3>Qu’est-ce que sont les TSA ? et pourquoi parle-t-on de « Spectre de l’Autisme » ?</h3>
+
+                                ​
+
+                                <p>Les TSA sont caractérisés par une difficulté dans les relations et la communication sociale ; il existe des intérêts forts et marqués pour des domaines spécifiques ; de même une sensorialité exacerbée (en hypo ou hyper)<br>
+
+                                Parce qu’il n'existe pas un type d'autisme, mais de multiples formes et présentations. On parle d’hétérogénéité des formes cliniques. Les TSA sont des troubles neurodéveloppementaux chroniques dont le début est durant l’enfance.</p>
+                                
+
+                                <h3>Pourquoi un diagnostic ?</h3>
+
+                                ​
+
+                                <p>Rechercher un TSA n'est pas une démarche facile et/ou anodine. Il faut qu'il y ait un objectif à ce diagnostic, dans votre vie personnelle, professionnelle ou affective (ou les trois, bien sûr !).<br>
+
+                                Un diagnostic se fait sur la base d'un regroupement de conduites trop intenses et/ou fréquentes et qui posent problèmes tant à l'enfant (l'ado ou l'adulte) qu'à sa famille, dans la vie de tous les jours, dans la scolarité et/ou le travail.<br>
+
+                                Je dis toujours que le diagnostic est une « ouverture » pas une étiquette et qu’il doit permettre la reconnaissance des droits de la personne, enfant ou adulte.</p>
+
+                                ​
+
+                                <h3>Quels outils pour le diagnostic ? </h3>
+
+                                ​
+
+                                <p>Je pratique l’ADI-R, outil clinique reconnu au plan international pour mettre en évidence les TSA. C’est un entretien qu’on dit rétrospectif, donc sur toute la vie, de la grossesse de la mère au moment actuel. C’est pour cela que la présence d’une personne vous ayant connu petit(e) est très fortement recommandée ; c’est un gage de fiabilité du diagnostic.<br>
+
+                                Les personnes adultes qui viennent, veulent être sûre, cela se comprend, qu’on va vers le bon diagnostic : donc le plus de renseignements possibles est souhaitable !</p>
+
+                                ​
+
+                                <p><em><span>L’ADI-R dure 3h voire 3h30. Il faut prévoir une demi-journée.</span></em></p>
+
+                                ​
+
+                                <p>Je fais une échelle de sensorialité (Dunn), le DSM5 qui est obligatoire. J’ai d’autres échelles à adapter selon les cas.<br>
+
+                                
+
+                                Si vous faites un diagnostic avec un(e ) psychologue et que vous souhaitez un dossier MDPH (Maison du Handicap), il faut la validation donc soit d'un médecin ou d'un psychiatre. Mais sinon, la reconnaissance est la même.</p>
+
+                                ​
+
+                                <p><strong>Enfin, je pratique le test WAIS IV qui est le bilan cognitif chez les adultes de plus de 16 ans ; on appelle cela QI, qui n’est d’ailleurs pas un terme adapté ! C’est en effet bien plus qu’un simple score, plutôt une manière de mettre en évidence les processus cognitifs requis dans la vie de tous les jours et dans les taches professionnelles ou les études.</strong></p>
+
+                                ​
+                                <div class="article-links">
+
+                                    <h4>Liens d’associations avec lesquelles je suis en relation:</h4>
+
+                                    <p><meta><strong>Asperger Amitiés</strong> :
+                                    
+                                    <br><a href="https://www.asperger-amitie.com/" target="_blank">www.asperger-amitie.com</a></meta>
+                                    <br>info@aspergeramitie.com
+                                    <br> 
+
+                                    <p><meta><strong>Association Francophone des Femmes Autistes</strong> :<br>
+                                    <a href="https://femmesautistesfrancophones.com/" target="_blank"> www.femmesautistesfrancophones.com</a></meta></p><br>
+                                    </p>
+
+                                </div>
+
                             </article>
 
-                            <a href="index.php"> Back </a>
+                            
                             
                         </div>
                     </div>
